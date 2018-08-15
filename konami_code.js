823980dd39a68a5a49d89c0692d16f887a925af5
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-15 22:49:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,10 +13,9 @@ const codes = [
 
 
 function init() {
-  console.log('is it working?')
   let index = 0;
   
-  function onKeyDownHandler(e) {
+  // function onKeyDownHandler(e) {
     const key = e.key;
    
     if (key === codes[index]) {
@@ -30,5 +29,5 @@ function init() {
     } else {
       index = 0;
     }
-  }
+  // }
 }
