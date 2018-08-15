abf42fe1309e439fdd514ed9d316afe553bfae20
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-15 22:43:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,5 +16,19 @@ function init() {
   // your code here
   let index = 0;
   
-  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+   
+    if (key === alphabet[index]) {
+      index++;
+   
+      if (index === alphabet.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
